6f0e584de2219254292d0c28c73ca0513523c806
isaiah <hal17038@byui.edu>
2021-02-27 20:50:35 -0700

    homework

--- week8/js/group.js
+++ week8/js/group.js
@@ -1,5 +1,7 @@
 const url = 'https://swapi.dev/api/people/'
-let nextURl = ''
+let nextURl = '';
+let prevURL = '';
+console.log(prevURL);
 
 function fetchData(pageCall) {
     fetch(pageCall)
@@ -8,20 +10,52 @@ function fetchData(pageCall) {
         console.log(jsonResult);
         const list = jsonResult['results'];
         const ol = document.querySelector('#json');
+        ol.innerHTML = '';
         for(let i = 0; i < list.length; i++) {
             ol.innerHTML +=
-            `<li>${list[i].name}</li>`
+            `
+            <li><a id='pressed' href="#">${list[i].name}</a></li>
+            <ul class = 'listing'>
+                <li>Height: ${list[i].height}</li>
+                <li>Mass: ${list[i].mass}</li>
+                <li>Hair Color: ${list[i].hair_color}</li>
+                <li>Skin Color: ${list[i].skin_color}</li>
+                <li>Eye Color: ${list[i].eye_color}</li>
+                <li>Birthdate: ${list[i].birth_year}</li>
+                <li>Gender: ${list[i].gender}</li>
+                <li>Homeworld: ${list[i].homeworld}</li>
+            </ul>
+            `
         }
-        nextURL = jsonResult.next;
-        console.log(nextURL);
+        if(jsonResult.next != null){
+            nextURL = jsonResult.next;
+        }
+        else{
+            nextURl = null;
+        }
+        if(jsonResult.previous != null){
+            prevURL = jsonResult.previous;
+        }
+        else{
+            prevURL = null;
+        }
     })
 }
 fetchData(url);
 
+const listing = document.querySelector('.listing');
 
     const prev = document.querySelector('#prev');
     const next = document.querySelector('#next');
 
     next.addEventListener('click', () => {
-        fetchData(nextURL);
+        if(nextURl != null){
+            fetchData(nextURL);
+        }
     })
+    prev.addEventListener('click', () => {
+        if(prevURL != null){
+            fetchData(prevURL);
+        }
+    })
+    
