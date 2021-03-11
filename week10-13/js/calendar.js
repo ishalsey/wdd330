d732c26cb6b3a05a75b923fd3205fbd7e6145758
isaiah <hal17038@byui.edu>
2021-03-11 14:28:06 -0700

    finalProject_input

--- week10-13/js/calendar.js
+++ week10-13/js/calendar.js
@@ -1,16 +1,26 @@
 
 let d = new Date();
 const calendars = document.querySelector('#monthView');
+const adds = document.querySelector('#add');
+const info = document.querySelector('#info');
 
+adds.addEventListener('click', (e) => {
+    if(info.classList != 'input') {
+        info.classList.add('input');
+    }
+    else {
+        info.classList.remove('input')
+    }
+})
 
 const getDaysInMonth = (month, year) => {
     return new Date(year, month, 0).getDate();
 }
 console.log(getDaysInMonth(3,2021));
 
-function print(){
+function date(){
     for(i = 1; i <= getDaysInMonth(3,2021); i++){
-        calendars.innerHTML += `<div class='days'>${i}</div>`
+        calendars.innerHTML += `<div class='days'><p>${i}</p></div>`
     }
 }
-print();
+date();
