949040cdb5257559dfd54a317bfe491f1fac4df4
ishalsey <hal17038@byui.edu>
2021-03-13 21:20:51 -0700

    group

--- week10/js/index.js
+++ week10/js/index.js
@@ -1,5 +1,8 @@
-import { getJSON, getLocation } from 'utilities.js';
+import { getJSON, getLocation } from './utilities.js';
 
 const baseUrl ='https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
-import QuakesController from './quakesController'
+import QuakesController from './quakesController.js';
+
+let myQuakesController = new QuakesController("#quakeList");
+myQuakesController.init();
